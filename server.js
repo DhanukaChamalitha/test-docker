c85f87ebc3d301bf9747b91c90694dc39b3b1a2f
DhanukaChamalitha <dhanukawickramasinghe1024@gmail.com>
2025-01-24 10:47:12 +0530

    web hook

--- server.js
+++ server.js
@@ -11,5 +11,8 @@ app.get("/hello", (req,res)=>{
 app.get("/test", (req,res)=>{
     res.send('{"response": "test,CICD"}')
 })
+app.get("/hook", (req,res)=>{
+    res.send('{"response": "test,trigger"}')
+})
 
 app.listen(process.env.PORT || 3000, ()=>console.log("Server up and runnign on port 3000"))
