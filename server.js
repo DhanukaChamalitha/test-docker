072026b15c4ac06bcf526fa9072238d9fd808102
Dhanuka Chamalitha <155315097+DhanukaChamalitha@users.noreply.github.com>
2025-01-24 10:49:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/test", (req,res)=>{
     res.send('{"response": "test,CICD"}')
 })
 app.get("/hook", (req,res)=>{
-    res.send('{"response": "test,trigger"}')
+    res.send('{"response": "test,hook"}')
 })
 
 app.listen(process.env.PORT || 3000, ()=>console.log("Server up and runnign on port 3000"))
